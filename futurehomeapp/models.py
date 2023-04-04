ce97753eb26dd518698c2665b1344ad27ec40f1f
ValeP314 <valeriaparisani@hotmail.com>
2023-04-04 20:36:28 +0000

    Create Listing and Features models

--- futurehomeapp/models.py
+++ futurehomeapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, "For Sale"), (1, "Sold"))
+
+
+class Listing(models.Model):
+    address = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listing_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(
+        User, related_name='listing_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.address
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Features(models.Model):
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="features")
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    ber_category = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
