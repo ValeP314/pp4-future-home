9e75624cf0babfe0d457dd1b18c01392e722e6a6
ValeP314 <valeriaparisani@hotmail.com>
2023-04-09 19:24:21 +0000

    Added templates and modified basic html and css for navigation bar and footer

--- futurehomeapp/views.py
+++ futurehomeapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Listing
 
-# Create your views here.
+
+class ListingList(generic.ListView):
+    model = Listing
+    queryset = Listing.objects.filter(status=0).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
